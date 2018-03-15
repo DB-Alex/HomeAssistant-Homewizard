d8f24c77fb123c7b0cd3d5ef86b6c42d54235316
Alex van der Vegt <alexvdvegt@vegtmedia.com>
2018-03-15 12:12:36 +0100

    A few changes to clean up the logfile

--- homewizard.py
+++ homewizard.py
@@ -1,20 +1,9 @@
-"""
-This component wouldn't be possible without the massive amount of inspiration I got from the following scripts:
-https://github.com/cyberjunky/home-assistant-custom-components/blob/master/climate/toon.py
-https://github.com/rvdvoorde/domoticz-homewizard
-
-This component is far from done yet, but it'll do for now.
-"""
-from homeassistant.const import TEMP_CELSIUS
-from homeassistant.helpers.entity import Entity
-from homeassistant.components.climate import (ClimateDevice, PLATFORM_SCHEMA, SUPPORT_TARGET_TEMPERATURE)
-from homeassistant.const import (CONF_NAME, CONF_HOST, CONF_PASSWORD,
-                                 TEMP_CELSIUS, ATTR_TEMPERATURE)
+from homeassistant.components.climate import (ClimateDevice, PLATFORM_SCHEMA, SUPPORT_TARGET_TEMPERATURE, STATE_AUTO)
+from homeassistant.const import (CONF_HOST, CONF_PASSWORD, TEMP_CELSIUS, ATTR_TEMPERATURE)
 import homeassistant.helpers.config_validation as cv
 
+import requests
 import json
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
 import logging
 import voluptuous as vol
 
@@ -22,79 +11,44 @@ SUPPORT_FLAGS = SUPPORT_TARGET_TEMPERATURE
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = 'Homewizard Thermostat'
-DEFAULT_TIMEOUT = 2
+DEFAULT_TIMEOUT = 15
 BASE_URL = 'http://{0}/{1}/{2}'
 
-ATTR_MODE = 'Heating'
+ATTR_PUMP = 'pump'
+ATTR_DHW = 'dhw'
+ATTR_WATER_TEMP = 'water_temperature'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
     vol.Required(CONF_HOST): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
 })
 
-
 def setup_platform(hass, config, add_devices, discovery_info=None):
-    """Setup the sensor platform."""
-    add_devices([homewizard(config.get(CONF_NAME), config.get(CONF_HOST), config.get(CONF_PASSWORD))])
+    """Set up the Heatlink thermostat."""
+    _LOGGER.debug("Heatlink config: %s" % config)
 
+    add_devices(
+        [Heatlink(config.get(CONF_HOST), config.get(CONF_PASSWORD))],
+        True
+    )
 
-class homewizard(ClimateDevice):
-    """Representation of a Sensor."""
+    return True
 
-    def __init__(self, name, address, password):
-        """Initialize the sensor."""
-        self._name = name
-        self._address = address
+class Heatlink(ClimateDevice):
+    """Representation of a Heatlink thermostat."""
+    def __init__(self, hostname, password):
+        """Setup Homewizard details"""
+        self._hostname = hostname
         self._password = password
-        self._current_temp = None
-        self._current_setpoint = None
-        self._current_state = 'off'
-        _LOGGER.debug("Init called")
-        self.update()
 
-    @staticmethod
-    def connect(order):
-        try:
-            url = Request(order)
-            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-
-        except HTTPError as e:
-            _LOGGER.exception("The server couldn\'t fulfill the request. Error code: ", e.code)
-            return "error"
-
-        except URLError as e:
-            _LOGGER.exception("We failed to reach a server. Reason: ", e.reason)
-            return "error"
-
-        else:
-            urloutput = response.read().decode("utf-8", "ignore")
-            jsonoutput = json.loads(urloutput)
-            return jsonoutput
-
-    @property
-    def should_poll(self):
-        """Polling needed for thermostat."""
-        _LOGGER.debug("Should_Poll called")
-        return True
-
-    def update(self):
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        _LOGGER.debug("Update called")
-        getlist = self.connect(BASE_URL.format(self._address, self._password, "get-sensors"))
-        if getlist != 'error':
-            heatlink = dict(getlist['response']['heatlinks'][0])
-            self._current_temp = round(heatlink['rte'], 1)
-            self._current_setpoint = round(heatlink['tte'], 1)
-            self._current_state = heatlink['heating']
-            _LOGGER.debug("Update successful")
-        else:
-            _LOGGER.exception("Update failed")
-            
+        """Initialize the thermostat."""
+        self._name = 'Heatlink'
+        self._current_temperature = None
+        self._target_temperature = None
+        self._pump = None
+        self._dhw = None
+        self._heating = None
+        self._water_temperature = None
 
     @property
     def supported_features(self):
@@ -103,15 +57,8 @@ class homewizard(ClimateDevice):
 
     @property
     def name(self):
-        """Return the name of the thermostat."""
+        """Return the name of the heatlink, if any."""
         return self._name
-
-    @property
-    def device_state_attributes(self):
-        """Return the device specific state attributes."""
-        return {
-            ATTR_MODE: self._current_state
-        }
 
     @property
     def temperature_unit(self):
@@ -121,29 +68,96 @@ class homewizard(ClimateDevice):
     @property
     def current_temperature(self):
         """Return the current temperature."""
-        return self._current_temp
+        return self._current_temperature
 
     @property
     def target_temperature(self):
         """Return the temperature we try to reach."""
-        return self._current_setpoint
+        return self._target_temperature
 
     @property
     def current_operation(self):
         """Return the current state of the thermostat."""
-        state = self._current_state
-        return state
+        return STATE_AUTO
+
+    @staticmethod
+    def _call_client(url):
+        """Call the Homewizard API"""
+        try:
+            client = requests.get(url, timeout=DEFAULT_TIMEOUT)
+        except:
+            _LOGGER.error("Error calling client: %s", client.status_code)
+            return False
+
+        return json.loads(client.text)
 
     def set_temperature(self, **kwargs):
         """Set new target temperature."""
         temperature = kwargs.get(ATTR_TEMPERATURE)
         if temperature is None:
             return
+        self._call_client(BASE_URL.format(
+            self._hostname, 
+            self._password, 
+            'hl/0/settarget/' + str(temperature)
+        ))
+
+    @property
+    def device_state_attributes(self):
+        """Return the device specific state attributes."""
+        return {
+            ATTR_PUMP: (self._pump and 'on' or 'off'),
+            ATTR_DHW: (self._dhw and 'on' or 'off'),
+            ATTR_WATER_TEMP: self._water_temperature,
+        }
+
+    def _refresh(self):
+        """Refresh the state attributes."""
+        attributes = self._call_client(BASE_URL.format(
+            self._hostname, 
+            self._password, 
+            'get-sensors'
+        ))
+        response = attributes['response']['heatlinks'][0];
+
+        self._name = response['name']
+        self._current_temperature = round(response['rte'], 1)
+        self._pump = response['pump']
+        self._dhw = response['dhw']
+        self._heating = response['heating']
+        self._water_temperature = round(response['wte'], 1)
+
+        tte = round(response['tte'], 1)
+        rsp =  round(response['rsp'], 1)
+
+        if int(tte) is 0:
+            self._target_temperature = rsp
         else:
-            settemp = self.connect(BASE_URL.format(self._address, self._password, "hl/0/settarget/" + str(temperature)))
-            if settemp != 'error':
-                _LOGGER.debug("Set temperature=%s", str(temperature))
-            else:
-                _LOGGER.exception("Error setting Heatlink")
-        # Not sure if needed
-        # self.update()
+            self._target_temperature = tte
+
+        return True
+
+    def update(self):
+        """Update the state."""
+        retries = 3
+        while retries > 0:
+            try:
+                self._refresh()
+                break
+            except (OSError,
+                    requests.exceptions.ReadTimeout) as exp:
+                retries -= 1
+                if retries == 0:
+                    raise exp
+                if not self._retry():
+                    raise exp
+                _LOGGER.error("Heatlink update failed, Retrying - Error: %s", exp)
+
+    def _retry(self):
+        """Retry to update state"""
+        try:
+            self._refresh()
+        except:
+            return False
+       
+        return True
